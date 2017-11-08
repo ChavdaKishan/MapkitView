93bb876ab32722ddfdc81dfcad9d23daaa499110
iFlame <iflame@rd.local>
2017-11-08 10:57:53 +0530

    0811

--- Map_Kit/ViewController.swift
+++ Map_Kit/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
     
     @IBOutlet weak var Mapkit: MKMapView!
     
+    let locationmanager = CLLocationManager()
     let clLocation = CLLocation()
+    var myNewyourToJersey = MKRoute()
     
     override func viewDidLoad()
     {
@@ -37,12 +39,52 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
     
     @IBAction func FindRouteBtnPress(_ sender: UIButton)
     {
+        let pinobj1 = MKPointAnnotation()
+        pinobj1.coordinate = CLLocationCoordinate2DMake(42.360082, -71.058880)
+        pinobj1.title = "New york"
+        self.Mapkit.addAnnotation(pinobj1)
         
+        let pinobj2 = MKPointAnnotation()
+        pinobj2.coordinate = CLLocationCoordinate2DMake(42.373616, -71.109734)
+        pinobj2.title = "Jersey"
+        self.Mapkit.addAnnotation(pinobj2)
+        
+        self.Mapkit.centerCoordinate = pinobj2.coordinate
+        
+        let dirRequest = MKDirectionsRequest()
+        dirRequest.transportType = MKDirectionsTransportType.automobile
+        
+        dirRequest.source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2DMake(pinobj1.coordinate.latitude, pinobj1.coordinate.longitude)))
+        dirRequest.destination = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2DMake(pinobj2.coordinate.latitude, pinobj2.coordinate.longitude)))
+        
+        let direction = MKDirections(request: dirRequest)
+        direction.calculate(completionHandler: {
+            response, error in
+            print(response)
+            
+            if error == nil
+            {
+                self.myNewyourToJersey = (response?.routes[0])!
+                self.Mapkit.add(self.myNewyourToJersey.polyline)
+            }
+            else
+            {
+                print(error.debugDescription)
+            }
+        })
     }
     
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer
+    {
+        let renderobj = MKPolylineRenderer(polyline: self.myNewyourToJersey.polyline)
+        renderobj.strokeColor = UIColor.red
+        renderobj.lineWidth = 5
+        return renderobj
     }
 }
